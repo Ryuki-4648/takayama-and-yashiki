c79312b29649cc24b738b75dcfc255a8c78a5345
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2023-07-20 21:25:45 +0900

    クイズデータjson作成、マークアップ

--- quiz/src/App.tsx
+++ quiz/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import questionList from './data/question.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main className="w-2/3 mx-auto">
+        <p>10個の写真を見てどちらかを当ててください。</p>
+
+        <ul className="flex justify-center flex-wrap">
+          {questionList.map((item) => {
+            return (
+              <li className="flex justify-center flex-wrap w-full">
+                <img src={item.imagePath} alt="" />
+                <div className="flex">
+                  <button className="w-24 h-8">高山</button>
+                  <button className="w-24 h-8">屋敷</button>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
